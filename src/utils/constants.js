4a1cd2192818052ed04ca8a3fa0527117516ad77
praveen.dangwani <praveen.dangwani@clevertap.com>
2023-02-27 17:49:43 +0530

    Dynamic CT and Sc account ID and API Key

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,5 +5,5 @@ export const ctAccountAndRegions = [
   { label: "eu1", value: "566-7Z4-W54Z" },
   { label: "us1", value: "449-RRK-WW6Z" }]
 
-export const scAccountId = "61a52046f56a14cb19a1e9cc"
-export const scApikey = "9dcced09dae16c5e3606c22346d92361b77efdb360425913850bea4f22d812dd"
+// export const scAccountId = "61a52046f56a14cb19a1e9cc"
+// export const scApikey = "9dcced09dae16c5e3606c22346d92361b77efdb360425913850bea4f22d812dd"
