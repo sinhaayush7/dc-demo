08884a00f2a02799d059a221c8b810d0d06ed111
Ayush Sinha <97458726+sinhaayush7@users.noreply.github.com>
2023-03-15 07:22:57 +0000

    updated regions

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,9 +1,9 @@
 export const ctAccountAndRegions = [
-  { label: "in1" },
-  { label: "sk1"},
-  { label: "sg1" },
-  { label: "eu1" },
-  { label: "us1"}
+  { label: "in1", value: "in1" },
+  { label: "sk1", value: "sk1"},
+  { label: "sg1", value: "sg1" },
+  { label: "eu1", value: "eu1" },
+  { label: "us1", value: "us1"}
 ]
 
 // export const scAccountId = "61a52046f56a14cb19a1e9cc"
