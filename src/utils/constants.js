f6e4a923c15d27dda8003fea7bc1958d1aa25fd7
Ayush Sinha <ayush.sinha@clevertap.com>
2025-01-08 15:23:47 +0530

    updates calling credentials

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,10 +1,6 @@
 export const ctAccountAndRegions = [
-  { label: "in1", value: "in1" },
-  // { label: "sk1", value: "sk1"},
-  // { label: "sg1", value: "sg1" },
-  // { label: "eu1", value: "eu1" },
-  // { label: "us1", value: "us1"}
+  { label: "in1", value: "in1" }
 ]
 
-export const scAccountId = "61a52046f56a14cb19a1e9cc"
-export const scApikey = "9dcced09dae16c5e3606c22346d92361b77efdb360425913850bea4f22d812dd"
+export const scAccountId = "66b48a9db0b9d3bf00288b39"
+export const scApikey = "Fc1nRmyYeMLwXyj3Cq1VDzyT9TcVqpzQFgYnjmjLf4a85V2gavGpEL6uuAapT7zR"
